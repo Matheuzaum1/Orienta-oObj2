0e72d08b140df648e1fb0781c00cff1f3739b3db
Matheuzaum1 <matheushenriquerosendo@gmail.com>
2024-10-27 21:27:09 -0300

    Add pasta de estudos

---
+++ Estudos/TratamentoDeExcecoes/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+    //printsacktrace
+    public static void main(String[] args) throws Exception{
+        try {
+            System.out.println("mensagem antes da exceção");
+            int resultado = 10 / 0;
+            System.out.println("Mensagem depois da exceção");   
+        } catch (Exception e) {
+            System.out.println((e.getMessage()));
+        }finally{
+            //sempre sera executado
+            System.out.println("Fim do programa com sucesso");
+        }
+        System.out.println("Programa encerrado");
+  }
+}
